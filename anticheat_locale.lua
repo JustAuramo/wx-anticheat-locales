2de24e3db402ae32821a25a2f9f6deab888d7053
Snaily-Development-Finland <108193124+JustAuramo@users.noreply.github.com>
2023-09-05 23:05:36 +0300

    Add files via upload

---
+++ anticheat_locale.lua
@@ -0,0 +1,113 @@
+Locale = {}
+
+-- Do NOT touch the %s variable, it will break the locale!
+
+-- Webhooks
+Locale.NewDetection = "Havaittu Jotain Epäilyttävää"
+Locale.ConnBlocked = "Yhdistäminen Blockattu"
+Locale.StartedTitle = "Aloitettu"
+Locale.StartedDesc = "👀 Katsotaan Mahdollista Cheateria..."
+Locale.Connecting = "Pelaaja on yhdistämässä"
+Locale.Disconnected = "Pelaaja Poistui"
+
+-- Connecting card
+Locale.Title = "Yhdistetään..."
+Locale.SubTitle = "Tarkistetaan Bännejä"
+
+-- AntiVPN
+
+Locale.VPNDetected = "VPN/Proxy Havaittu"
+Locale.VPNSubtitle = "VPNs tai Proxies ei ole sallittuja serverillä!"
+Locale.VPNHelpText = "Ole hyvä ja poista VPN tai Proxy käytöstä ja kokeile yhdistää uudestaan. Sinun IP: %s"
+
+-- Detection Reasons
+-- Client
+Locale.AntiAFK = "Sinut on Kickattu, Afkasit liian pitkään!"
+Locale.AnticheatStopped = "AntiCheat didn't respond on client's side"
+Locale.BlacklistedWeapon = "Tämä Ase on Blacklistillä - [%s]"
+Locale.BlacklistedVeh = "Tämä Ajoneuvo on Blacklistillä - [%s]"
+Locale.Blips = "Pelaaja Blip Hack"
+Locale.NoClip = "No-Clip"
+Locale.VehiclePlate = "Blacklistattu auton rekisterikilpi - [%s]"
+Locale.PlateChange = "Changed his vehicle plate - [%s --> %s]"
+Locale.VehMods3 = "Vehicle Modifications [3]"
+Locale.VehMods2 = "Vehicle Modifications [2]"
+Locale.VehMods1 = "Vehicle Modifications [1]"
+Locale.FastRun = "Fast Run"
+Locale.GodMode = "God Mode"
+Locale.Spectate = "Spectate Mode Detected"
+Locale.NightVision = "Night Vision"
+Locale.ThermalVision = "Thermal Vision"
+Locale.MagicBullet = "Magic Bullets - [%s units]"
+Locale.AimAssist = "Aim Assist"
+Locale.DevTools = "NUI DevTools"
+Locale.Radar = "Minimap without being in vehicle"
+Locale.FreeCam = "Free Cam"
+Locale.Invisibility = "Invisibility"
+Locale.ExploAmmo = "Explosive Ammo"
+Locale.WepDmgBoost = "Citizen Cheats (AIs) - Weapon Damage Boost"
+Locale.MelDmgBoost = "Citizen Cheats (AIs) - Melee Damage Boost"
+Locale.WepDefBoost = "Citizen Cheats (AIs) - Weapon Defense Boost"
+Locale.MelDefBoost = "Citizen Cheats (AIs) - Melee Defense Boost"
+Locale.MaxHealth = "Maximum Health Reached"
+Locale.MaxArmor = "Maximum Armor Reached"
+Locale.SuperJump = "Super Jump"
+Locale.Stamina = "Infinite Stamina"
+Locale.InvisVehicle = "Invisible Vehicle"
+Locale.Pickup = "Pickup Detected"
+Locale.SmallPed = "Small Ped"
+Locale.Rainbow = "Rainbow Vehicle"
+Locale.ModMenu = "Mod Menu Detected"
+Locale.CitizenAIs = "Citizen Cheats (AIs)"
+Locale.ResStop = "Resurssi Pysäytetty - [%s]"
+Locale.ResStart = "Resurssi Aloitettu - [%s]"
+-- Server
+Locale.Trigger = "Blacklisted Trigger - [%s]"
+Locale.TriggerValue = "Blacklisted Trigger Value - [%s with %s]"
+Locale.Particle = "Blacklisted Particle - [%s]"
+Locale.Asset = "Blacklisted Asset - [%s]"
+Locale.Explosion = "Blacklisted Explosion - [Type %s]"
+Locale.ExplosionSpam = "Explosion Spam - [Created %s explosions in less than %s ms]"
+Locale.PedSpam = "Ped Spam - [Created %s peds in less than %s ms]"
+Locale.VehSpam = "Vehicle Spam - [Created %s vehicles in less than %s ms]"
+Locale.InaudibleExplosion = "Inaudible Explosion"
+Locale.InvisibleExplosion = "Invisible Explosion"
+Locale.BoostedExplosion = "Damage Boosted Explosion"
+Locale.Boats = "Boat Spawning"
+Locale.Vehicle = "Vehicle in Blacklist - [%s]"
+Locale.VehicleSpawn = "Spawned blacklisted vehicle - [%s]"
+Locale.Object = "Object in Blacklist - [%s]"
+Locale.Ped = "Blacklisted Ped - [%s]"
+Locale.Taser = "Taser Spam"
+Locale.ClearPedTasks = "Clear Ped Tasks"
+Locale.RemoveWeapons = "Removed Weapons"
+Locale.GiveWeapons = "Give Weapons"
+Locale.NoRagdoll = "No Ragdoll"
+Locale.ClipSize = "Overloaded Clip - [Had: %s | Maximum: %s]"
+Locale.OCR = "OCR Detection - [%s]"
+
+-- Kick/Ban Messages
+Locale.BlacklistedWord = "Sinut on Kickattu!\nSyy: Blacklisted Word(s)"
+Locale.Banned = "Sait bännit!\nSyy: %s\n\nBan ID: %s"
+Locale.BannedNoReason = "Sinut on bännätty!\nSinun Ban ID: %s" -- Only if wx.showReason is true
+Locale.Kicked = "Sinut on Kickattu!\nReason: %s"
+Locale.KickedNoReason = "Sinut on Kickattu!"
+
+Locale.NameNotAllowed = "🛡️ Sinun nimesi ei ole sallittu!\n🛡️ Nimesi sisältää [%s], mikä ei ole sallittua palvelimella!\n\n🛡️ Ole hyvä ja vaihda nimesi, koita sitten yhdistää uudelleen."
+
+Locale.DiscordNotFound = "Tarkista että Discord on päällä"
+Locale.DiscordReq = "Tämä palvelin vaatii että sinulla on Discord yhdistettynä!"
+Locale.DiscordGoTo = "Go to the FiveM settings and make sure that your Discord account is linked."
+
+-- Chat
+Locale.ChatPrefix = 'WX AntiCheat'
+Locale.Detected = "Pelaajalla %s on havaittu jotain! Havainto: %s"
+
+-- Banned Screen
+
+Locale.BannedKick = "Sinut on bännätty pysyvästi!\nSyy: %s\nSinun Ban ID: %s"
+Locale.Banned = "Sinulle on annettu pysyvä porttikielto!"
+Locale.Reason = "Syy"
+Locale.BanID = "Sinun Ban ID"
+Locale.BannedInfo = "Tämä porttikielto ei vanhene koskaan. Voit aina hakea Unban liittymällä Discordiin"
+Locale.SupportDiscord = "Discord"
